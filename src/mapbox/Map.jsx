074514992f2391576a98f894e5568d760e7c04d3
chiefkleef <cliftoncampbell360@gmail.com>
2017-03-03 19:26:01 -0800

    minor removal of map console logs

--- src/mapbox/Map.jsx
+++ src/mapbox/Map.jsx
@@ -231,7 +231,7 @@ export default class Map {
         this.map.setPaintProperty(layerName + '-fills-mimio', 'fill-opacity', 1);
         // this.map.getSource('selected').setData(gj.geojson);
       }
-    })
+    });
   }
 
   addSelectionPopup(feature, e) {
@@ -359,108 +359,3 @@ export default class Map {
   }
 
 }
-
-
-// drawTilesetLayers(geojsonTilesets) {
-//   // add all tilesets
-//   window.drawnLayers = []
-//   geojsonTilesets.forEach(tileset => {
-//     this.map.addSource(tileset.name, {
-//       type: 'vector',
-//       url: tileset.sourceUrl
-//     });
-//     var polyLayer = {
-//       id: tileset.name + '-fills',
-//       type: 'fill',
-//       source: tileset.name,
-//       'source-layer': tileset.sourceLayer,
-//       layout: {
-//         // visibility: 'none'
-//       },
-//       paint: {
-//         'fill-color': 'tomato',
-//         'fill-opacity': 0.5
-//       }
-//     }
-//     var lineLayer = {
-//       id: tileset.name + '-outlines',
-//       type: 'line',
-//       source: tileset.name,
-//       'source-layer': tileset.sourceLayer,
-//       layout: {
-//         // visibility: 'none'
-//       },
-//       paint: {
-//         'line-width': {
-//           'stops': [
-//             [10, .75],
-//             [11, 1],
-//             [12, 1.5],
-//             [13, 1.75],
-//             [14, 2]
-//           ]
-//         },
-//         'line-opacity': {
-//           'stops': [
-//             [10, 0.75],
-//             [11, 0.6],
-//             [12, 0.5],
-//             [13, 0.4]
-//           ]
-//         },
-//         'line-color': 'tomato'
-//       }
-//     }
-//     // active layers need to be known for querying purposes
-//     window.drawnLayers.push(tileset.name + '-fills', tileset.name + '-outlines')
-//     // second variable is the layer in the mapbox style you'd like the layers to be drawn directly above of
-//     this.map.addLayer(polyLayer, 'admin-2-boundaries-dispute');
-//     this.map.addLayer(lineLayer, 'admin-2-boundaries-dispute');
-//   });
-
-//   // add a dedicated source and layer to collect the currently hovered feature at all times
-//   this.map.addSource('hover', {
-//     type: 'geojson',
-//     data: geojsonNull
-//   });
-//   this.map.addLayer({
-//     id: 'polygon-hover',
-//     type: 'fill',
-//     source: 'hover',
-//     paint: {
-//       'fill-color': 'black',
-//       'fill-opacity': 0.10
-//     }
-//   }, 'admin-2-boundaries-dispute');
-
-//   // add a dedicated source and layer to collect the currently selected feature at all times
-//   this.map.addSource('selected', {
-//     type: 'geojson',
-//     data: geojsonNull
-//   });
-//   this.map.addLayer({
-//     id: 'polygon-selected',
-//     type: 'line',
-//     source: 'selected',
-//     paint: {
-//       'line-width': {
-//         'stops': [
-//           [10, 1.5],
-//           [11, 2],
-//           [12, 2.5],
-//           [13, 2.5],
-//           [14, 2.5]
-//         ]
-//       },
-//       'line-color': '#15f4ee',
-//       'line-opacity': 1,
-//     }
-//   }, 'admin-2-boundaries-dispute');
-
-//   // this feature can be nice for zooming to the data on load
-//   if (this.firstDraw) {
-//     this.zoomToDataExtent();
-//   }
-
-//   this.firstDraw = false;
-// }

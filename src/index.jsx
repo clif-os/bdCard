02eaebe9480e9747b49efbce4e601ae3272c88d8
chiefkleef <cliftoncampbell360@gmail.com>
2017-03-12 09:37:11 -0700

    cleaned up files

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,20 @@
 import './index.styl';
 import React from 'react';
-import {
-  render
-} from 'react-dom';
+import {render} from 'react-dom';
 require('velocity-animate');
 require('velocity-animate/velocity.ui');
 import AppInterface from './AppInterface.jsx';
-import './components/loadingPanes/MainLoadingPane.styl';
 import Map from './mapbox/Map.jsx';
-import {
-  linePaintIn,
-  fillPaintIn
-} from './mapbox/geojsonLayerUtils.jsx';
-import { convertGeojsonToLookup } from './utils/geojsonUtils.jsx'
+import {linePaintIn, fillPaintIn} from './mapbox/geojsonLayerUtils.jsx';
+import {convertGeojsonToLookup} from './utils/geojsonUtils.jsx'
 global.mapboxgl = require('mapbox-gl');
-import { dashboardListener } from './filter/filter.jsx'
+import {dashboardListener} from './filter/filter.jsx'
 
 import gjPropsMetadata from './data/jchs-boston-md.json';
-import {
-  convertGJLayersToLegendData
-} from './components/legend/legendUtils.jsx';
+import {convertGJLayersToLegendData} from './components/legend/legendUtils.jsx';
 
 // for the default visEvent
-import {
-  dollarFormatter
-} from './utils/generalUtils.jsx';
+import {dollarFormatter} from './utils/generalUtils.jsx';
 
 let url;
 
@@ -34,39 +24,45 @@ if (__DEV__) {
   url = 'http://panettone.herokuapp.com/json';
 }
 
-fetch(url).then(res => res.json()).then(gj => {
-  // for selections, it can often be useful to make a lookup tranformation of the geojsons at hand
-  // in order to perform rapid lookups --> things like selections and even zooms
-  window.activeFields = {};
-  window.geojsonLookup = convertGeojsonToLookup(gj);
-  window.geojson = gj;
-  const geojsonLayers = [{
-    geojson: gj,
-    name: 'inFilter',
-    filterStatus: 'Meets Filter Criteria',
-    linePaint: linePaintIn,
-    fillPaint: fillPaintIn
-  }]
-  const legendDescription = 'NULL';
-  const legendData = convertGJLayersToLegendData(geojsonLayers, legendDescription);
-  window.defaultVisEvent = {
-    "field": "MedINC14",
-    "classes": 4,
-    "palette": "red to green",
-    "unitFormatter": dollarFormatter,
-    "visActive": true
-  }
-  // empty window object for making selections
-  window.selectedFeature = null;
+fetch(url)
+  .then(res => res.json())
+  .then(gj => {
+    // for selections, it can often be useful to make a lookup tranformation of the
+    // geojsons at hand in order to perform rapid lookups --> things like selections
+    // and even zooms
+    window.activeFields = {};
+    window.geojsonLookup = convertGeojsonToLookup(gj);
+    window.geojson = gj;
+    const geojsonLayers = [
+      {
+        geojson: gj,
+        name: 'inFilter',
+        filterStatus: 'Meets Filter Criteria',
+        linePaint: linePaintIn,
+        fillPaint: fillPaintIn
+      }
+    ]
+    const legendDescription = 'NULL';
+    const legendData = convertGJLayersToLegendData(geojsonLayers, legendDescription);
+    window.defaultVisEvent = {
+      "field": "MedINC14",
+      "classes": 4,
+      "palette": "red to green",
+      "unitFormatter": dollarFormatter,
+      "visActive": true
+    }
+    // empty window object for making selections
+    window.selectedFeature = null;
 
-  mapboxgl.accessToken = 'pk.eyJ1IjoiY2FtcC1hbW9zIiwiYSI6ImNpcjg0cTJvMzAweThnZG5rY2Znazhnc2kifQ.jLCXm1LQmHyDC2RaFTBJNA';
-  const mapStyle = 'mapbox://styles/mapbox/streets-v9' // streets style
-  // const mapStyle = 'mapbox://styles/mapbox/outdoors-v9' //outdoors style
-  // const mapStyle="mapbox://styles/camp-amos/ciwn0ej5z00402pnxt5t42d4o"; //light style
-  // const mapStyle = "mapbox://styles/camp-amos/cirmc9juf002hg1nboacfr7u9"; // dark style
-  dashboardListener();
-  const m = new Map(geojsonLayers, mapStyle);
-  render(<AppInterface propsMd = {gjPropsMetadata} legendData = {legendData} /> ,
-    document.getElementById('AppInterface')
-  );
-});
+    mapboxgl.accessToken = 'pk.eyJ1IjoiY2FtcC1hbW9zIiwiYSI6ImNpcjg0cTJvMzAweThnZG5rY2Znazhnc2kifQ.jLCXm1LQmH' +
+        'yDC2RaFTBJNA';
+    const mapStyle = 'mapbox://styles/mapbox/streets-v9' // streets style
+    // const mapStyle = 'mapbox://styles/mapbox/outdoors-v9' //outdoors style const
+    // mapStyle="mapbox://styles/camp-amos/ciwn0ej5z00402pnxt5t42d4o"; //light
+    // style const mapStyle = "mapbox://styles/camp-amos/cirmc9juf002hg1nboacfr7u9";
+    // // dark style
+    dashboardListener();
+    const m = new Map(geojsonLayers, mapStyle);
+    render(
+      <AppInterface propsMd={gjPropsMetadata} legendData={legendData}/>, document.getElementById('AppInterface'));
+  });

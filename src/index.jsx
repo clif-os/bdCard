a6cf75f4f00443e3dae9a25fcfcd47f1f4db0678
chiefkleef <cliftoncampbell360@gmail.com>
2017-05-10 16:28:05 -0700

    updated data and added in new recommendations

--- src/index.jsx
+++ src/index.jsx
@@ -63,7 +63,7 @@ fetch(url)
       "visActive": true
     }
     // loading activeVisFields with a default value that is represented in a superficial visualization event from the Map.jsx
-    window.defaultActiveFields = {MedInc_14:"Median Household Income 2014", TractPopulation2010: "Tract Population 2010"};
+    window.defaultActiveFields = {MedINC14:"Median Household Income 2014", TractPopulation2010: "Tract Population 2010"};
     window.activeFields = Object.assign({}, window.defaultActiveFields);
     window.activeVisFields = {};
     // window.activeFields = {MedINC14:"Median Household Income 2014", TractPopulation2010: "Tract Population 2010"};

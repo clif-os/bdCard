4207276b64a21a01f81f41b5089c5baeb746be7b
chiefkleef <cliftoncampbell360@gmail.com>
2017-06-16 09:05:25 -0700

    using new prop-types package

--- src/components/mapShowcaser/mapSelector/MapSelector.jsx
+++ src/components/mapShowcaser/mapSelector/MapSelector.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import './MapSelector.styl';
 import { toggleSelectorOpen } from '../../../actions/index.jsx';

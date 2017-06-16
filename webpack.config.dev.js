1371c6130ee3c48cba380c7fa3b8cfce42f5d801
eltonjuan <mikeamos@gmail.com>
2017-06-16 00:41:19 -0700

    fixing dev config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,22 +1,19 @@
-const path = require('path');
+var path = require('path');
 const webpack = require('webpack');
-const pkg = require('./package.json');
+
 module.exports = {
   context: __dirname,
-  devtool: 'cheap-module-source-map',
+  devtool: 'eval-cheap-module-source-map',
   entry: {
     bundle: './src/index.jsx',
     vendor: ['react', 'mapbox-gl']
   },
-  output: {
-    path: path.resolve(__dirname, 'src'),
-    publicPath: '/',
-    filename: '[name].js'
-  },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: [
+      '', '.js'
+    ],
     alias: {
-      webworkify: 'webworkify-webpack',
+      'webworkify': 'webworkify-webpack',
       'mapbox-gl': path.resolve('./node_modules/mapbox-gl/dist/mapbox-gl.js')
     },
     modules: ['node_modules', 'src']
@@ -24,50 +21,48 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
+        NODE_ENV: JSON.stringify('development')
       },
-      __DEV__: false,
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false,
-      },
-    }),
+      '__DEV__': true
+    })
   ],
+  output: {
+    path: path.join(__dirname, 'src/'),
+    publicPath: 'src/', // relative path for github pages
+    filename: '[name].js', // no hash in main.js because index.html is a static page
+    chunkFilename: '[hash]/js/[id].js',
+    hotUpdateMainFilename: '[hash]/update.json',
+    hotUpdateChunkFilename: '[hash]/js/[id].update.js'
+  },
   module: {
-    loaders: [{
-      test: /\.jsx$/,
-      exclude: 'node_modules',
-      loader: 'babel',
-      query: {
-        presets: ['es2015', 'react', 'stage-0']
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: 'node_modules',
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react', 'stage-0']
+        }
+      }, {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }, {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'node_modules/webworkify/index.js'),
+        loader: 'worker'
+      }, {
+        test: /mapbox-gl.+\.js$/,
+        loader: 'transform/cacheable?brfs'
+      }, {
+        test: /\.styl$/,
+        loader: 'style-loader!css-loader!stylus-loader'
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }, {
+        test: '/\.(jpg|png)$/', //eslint-disable-line
+        loader: 'url-loader?mimetype=image/png'
       }
-    },
-    {
-      test: /\.json$/,
-      loader: 'json-loader'
-    },
-    {
-      test: /\.js$/,
-      include: path.resolve(__dirname, 'node_modules/webworkify/index.js'),
-      loader: 'worker'
-    },
-    {
-      test: /mapbox-gl.+\.js$/,
-      loader: 'transform/cacheable?brfs'
-    },
-    {
-      test: /\.styl$/,
-      loader: 'style-loader!css-loader!stylus-loader'
-    },
-    {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    },
-    {
-      test: '/\.(jpg|png)$/', //eslint-disable-line
-      loader: 'url-loader?mimetype=image/png'
-    }
     ]
   },
   node: {

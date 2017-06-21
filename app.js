07f117df7c75435efabd876bc291d2ffcdac015c
chiefkleef <cliftoncampbell360@gmail.com>
2017-06-20 22:57:20 -0700

    added heroku speicifications to the package

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+
+const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.use('/', express.static('src'));
-// app.use('/', express.static('src'));
+app.use('/static', express.static('static'));
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  //intercepts OPTIONS method
-  if ('OPTIONS' === req.method) {
-    //respond with 200
+  // intercepts OPTIONS method
+  if (req.method === 'OPTIONS') {
+    // respond with 200
     res.send(200);
   } else {
-    //move on
+    // move on
     next();
   }
 });
 
-var server = app.listen(app.get('port'), () => {
-  var port = server
+const server = app.listen(app.get('port'), () => {
+  const port = server
     .address()
     .port;
-  console.log('server listening on port ', port)
+  console.log('server listening on port ', port);
 });
